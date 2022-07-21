ccdcec1f7427e8977889a995cfb7a6ebdaf7121d
Germano Kuerten <germanokuerten@gmail.com>
2022-07-20 22:29:19 -0700

    Book model created in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+# Django built a parent class called models that has a bunch of functionality built in.
+
+# Book Model
+# Name, author, description, price
+
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    price = models.IntegerField()
