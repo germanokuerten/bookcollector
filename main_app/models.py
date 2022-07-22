c271fa59c9b21e7f423a1867b7113f209315c1b4
Germano Kuerten <germanokuerten@gmail.com>
2022-07-22 01:24:24 -0700

    added data bookcollector

--- main_app/models.py
+++ main_app/models.py
@@ -6,3 +6,12 @@ from django.db import models
 # Name, author, description, price
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    price = models.IntegerField()
+
+    def __str__(self):
+        # return self.name
+        return f"{self.name} - {self.author}"
